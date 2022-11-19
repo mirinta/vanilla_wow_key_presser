3b1cc3687d89506c3d79025e086e27d88fac7c95
mirinta <mirinta@163.com>
2022-11-20 02:30:20 +0800

    feat: disable window maximize button

--- src/ui/MainWindow.py
+++ src/ui/MainWindow.py
@@ -20,7 +20,7 @@ class MainWindow(QMainWindow):
         icon = QIcon(os.path.join(os.path.dirname(__file__), "../resource/icon.png"))
         self.setWindowIcon(icon)
         self.setWindowTitle("Vanilla WoW Battleground Bot")
-        self.setWindowFlags(self.windowFlags() | Qt.WindowStaysOnTopHint)
+        self.setWindowFlags(self.windowFlags() & (~Qt.WindowMaximizeButtonHint))
         self.setMinimumSize(350, 350)
         self.setCentralWidget(QWidget(self))
         main_layout = QVBoxLayout()
